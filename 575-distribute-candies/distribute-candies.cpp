d74dbf5509231939c0a7d8a92d7ffe44585f79fe
Karan Bhatt <karanbhatt160608@gmail.com>
2025-09-21 10:06:51 +0530

    Time: 47 ms (84.54%) | Memory: 85.4 MB (99.18%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        set<int> s;
-        for(int i=0;i<candyType.size();i++){
-            s.insert(candyType[i]);
+        sort(candyType.begin(),candyType.end());
+        int curr=candyType[0],count=1;
+        for(int i=1;i<candyType.size();i++){
+            if(candyType[i]!=curr){
+                count++;
+                curr=candyType[i];
+            }
         }
-        if(candyType.size()/2<s.size()) return candyType.size()/2;
-        return s.size();
+        if(candyType.size()/2<count) return candyType.size()/2;
+        return count;
     }
 };
