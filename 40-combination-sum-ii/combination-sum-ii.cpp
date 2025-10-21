5ad62e217f285322dd25bc4d3af1c79131326723
Karan Bhatt <karanbhatt160608@gmail.com>
2025-10-21 19:49:06 +0530

    Time: 4 ms (24.66%) | Memory: 14.4 MB (22.74%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+void solve(vector<int>& candidates, int target, int idx, set<vector<int>>& ans, vector<int>& v){
+    if(target<0){
+        return;
+    }
+    if(target==0){
+        ans.insert(v);
+        return;
+    }
+    for(int i=idx;i<candidates.size();i++){
+        if(i!=idx && candidates[i]==candidates[i-1]){
+            continue;
+        }
+        v.push_back(candidates[i]);
+        solve(candidates,target-candidates[i],i+1,ans,v);
+        v.pop_back();
+    }
+    return;
+}
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        set<vector<int>> ans;
+        vector<int> v;
+        solve(candidates,target,0,ans,v);
+        vector<vector<int>> result(ans.begin(),ans.end());
+        return result;
+    }
+};
