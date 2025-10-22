78fe5b6f6a2bc8ab5353dcda83334862e64d2d71
Karan Bhatt <karanbhatt160608@gmail.com>
2025-10-22 11:50:57 +0530

    Time: 11 ms (85.01%) | Memory: 169.5 MB (98.60%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.cpp
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n, vector<int>& costs) {
-        vector<int> result(n+1,0);
+        vector<int> result(4,0);
         result[1]=costs[0]+1;
         if(n==1) return result[1];
         result[2]=min(costs[1]+4,costs[1]+result[1]+1);
+        if(n==2) return result[2];
         for(int i=3;i<=n;i++){
-            result[i]=min(result[i-3]+costs[i-1]+9,min(result[i-2]+costs[i-1]+4,result[i-1]+costs[i-1]+1));
+            result[3]=min(result[0]+costs[i-1]+9,min(result[1]+costs[i-1]+4,result[2]+costs[i-1]+1));
+            result[0]=result[1];
+            result[1]=result[2];
+            result[2]=result[3];
         }
-        return result[n];
+        return result[3];
     }
 };
