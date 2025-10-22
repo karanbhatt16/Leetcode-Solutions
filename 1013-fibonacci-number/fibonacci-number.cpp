120c44ed959b1506dce246fefdd1d8698b2ac453
Karan Bhatt <karanbhatt160608@gmail.com>
2025-10-22 09:44:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.56%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     int fibonacci(int n, vector<int>& v){
         if(n<=1) return n;
-        int left,right;
-        if(v[n-1] != -1) left=v[n-1];
-        else left=fibonacci(n-1,v);
-        if(v[n-2] != -1) right=v[n-2];
-        else right=fibonacci(n-2,v);
-        v[n]=left+right;
-        return left+right;
+        if(v[n]!=-1) return v[n];
+        v[n]=fibonacci(n-1,v)+fibonacci(n-2,v);
+        return v[n];
     }
     int fib(int n) {
         vector<int> v(n+1,-1);
