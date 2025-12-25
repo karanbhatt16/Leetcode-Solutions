dfcb5647e5125e66a298085b72060d9e3fa8f87e
Karan Bhatt <karanbhatt160608@gmail.com>
2025-12-25 20:24:11 +0530

    Time: 3 ms (32.00%) | Memory: 13.2 MB (98.11%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -7,7 +7,7 @@ public:
             for(int j=0;j<nums.size();j++){
                 if(nums[j]&(1<<i)) sum+=1;
             }
-            if(sum%3!=0) ans=ans+(1<<i);
+            if(sum%3!=0) ans=ans|(1<<i);
         }
         return ans;
     }
