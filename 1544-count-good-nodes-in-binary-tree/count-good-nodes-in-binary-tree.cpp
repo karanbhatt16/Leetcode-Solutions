5fb77cf25d10bc1b077b521fceb818c93bb495a9
Karan Bhatt <karanbhatt160608@gmail.com>
2025-12-27 19:16:31 +0530

    Time: 102 ms (39.19%) | Memory: 88.4 MB (61.45%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -13,10 +13,6 @@ class Solution {
 public:
     int solve(TreeNode* root, int m){
         if(root==nullptr) return 0;
-        if(root->left==nullptr && root->right==nullptr){
-            if(root->val>=m) return 1;
-            else return 0;
-        }
         int ans=solve(root->left, max(m,root->val))+solve(root->right, max(m,root->val));
         if(root->val>=m) ans+=1;
         return ans;
