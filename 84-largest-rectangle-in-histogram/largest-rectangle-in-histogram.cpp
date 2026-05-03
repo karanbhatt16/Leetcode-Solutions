4fe5628b2f5dc2a6754b551827c2969141ada611
Karan Bhatt <karanbhatt160608@gmail.com>
2026-05-03 13:06:23 +0530

    Time: 28 ms (32.94%) | Memory: 87.5 MB (16.33%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,47 +1,24 @@
 class Solution {
-private:
-
-    vector<int> nse(vector<int>& heights) {
-        stack<int> st;
-        int n = heights.size();
-        vector<int> ans(n);
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) {
-                st.pop();
-            }
-            if (st.empty()) ans[i] = n;
-            else ans[i] = st.top();
-
-            st.push(i);
-        }
-        return ans;
-    }
-
-    vector<int> pse(vector<int>& heights) {
-        stack<int> st;
-        int n = heights.size();
-        vector<int> ans(n);
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) {
-                st.pop();
-            }
-            if (st.empty()) ans[i] = -1;
-            else ans[i] = st.top();
-
-            st.push(i);
-        }
-        return ans;
-    }
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> left = pse(heights);
-        vector<int> right = nse(heights);
-        int maxarea = 0;
+        stack<int> st;
+        int ans = 0;
+        vector<int> pse;
         for (int i = 0; i < n; i++) {
-            int area = (right[i] - left[i] - 1) * heights[i];
-            maxarea = max(maxarea, area);
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                ans = max(ans, heights[st.top()] * (i - pse[st.top()] - 1));
+                st.pop();
+            }
+
+            if (st.empty()) pse.push_back(-1);
+            else pse.push_back(st.top());
+            st.push(i);
         }
-        return maxarea;
+        while (!st.empty()) {
+            ans = max(ans, heights[st.top()] * (n - 1 - pse[st.top()]));
+            st.pop();
+        }
+        return ans;
     }
 };
