0930364dfb23920cbc899f31f9abd64583d946ca
Karan Bhatt <karanbhatt160608@gmail.com>
2026-06-07 08:46:28 +0530

    Time: 277 ms (21.51%) | Memory: 300.8 MB (18.66%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, int> left;
+        unordered_map<int, int> right;
+        set<int> s;
+
+        int n = descriptions.size();
+
+        for (int i = 0; i < n; i++) {
+            if (descriptions[i][2] == 1) {
+                left[descriptions[i][0]] = descriptions[i][1];
+            } else {
+                right[descriptions[i][0]] = descriptions[i][1];
+            }
+            s.insert(descriptions[i][1]);
+        }
+
+        int rootval;
+        
+        for (int i = 0; i < n; i++) {
+            if (s.find(descriptions[i][0]) == s.end()) {
+                rootval = descriptions[i][0];
+                break;
+            }
+        }
+
+        queue<TreeNode*> q;
+        TreeNode* root = new TreeNode(rootval);
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            if (left[node->val] > 0) {
+                TreeNode* leftnode = new TreeNode(left[node->val]);
+                node->left = leftnode;
+                q.push(leftnode);
+            }
+            if (right[node->val] > 0) {
+                TreeNode* rightnode = new TreeNode(right[node->val]);
+                node->right = rightnode;
+                q.push(rightnode);
+            }
+        }
+
+        return root;
+
+    }
+};
