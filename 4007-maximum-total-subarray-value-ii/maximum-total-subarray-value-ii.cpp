5444c034840c79f6f91b3c0bb97a142cef28581c
Karan Bhatt <karanbhatt160608@gmail.com>
2026-06-10 22:58:19 +0530

    Time: 650 ms (62.09%) | Memory: 162.9 MB (73.39%) - LeetSync

---
+++ 4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.cpp
@@ -0,0 +1,98 @@
+#define ll long long
+
+class SegmentTree {
+public:
+    vector<int> segmentTree;
+    bool isMinTree;
+
+    SegmentTree(vector<int>& nums, bool flag) {
+        int n = nums.size();
+        this->isMinTree = flag;
+
+        segmentTree.resize(4 * n);
+
+        buildSegmentTree(0, 0, n - 1, nums);
+    }
+
+
+    void buildSegmentTree(int i, int l, int r, vector<int>& nums) {
+        if (l == r) {
+            segmentTree[i] = nums[l];
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        buildSegmentTree(2 * i + 1, l, mid, nums);
+        buildSegmentTree(2 * i + 2, mid + 1, r, nums);
+
+        if (isMinTree) {
+            segmentTree[i] = min(segmentTree[2 * i + 1], segmentTree[2 * i + 2]);
+        } else {
+            segmentTree[i] = max(segmentTree[2 * i + 1], segmentTree[2 * i + 2]);
+        }
+    }
+
+    int querySegmentTree(int start, int end, int i, int l, int r) {
+        if (l > end || r < start) {
+            return isMinTree ? INT_MAX : INT_MIN;
+        }
+
+        if (l >= start && r <= end) {
+            return segmentTree[i];
+        }
+
+        int mid = l + (r - l) / 2;
+
+        int a = querySegmentTree(start, end, 2*i + 1, l, mid);
+        int b = querySegmentTree(start, end, 2*i + 2, mid + 1, r);
+
+        if (isMinTree) {
+            return min(a, b);
+        } else {
+            return max(a, b);
+        }
+    }
+
+    int query(int l, int r, int n) {
+        return querySegmentTree(l, r, 0, 0, n - 1);
+    }
+};
+
+class Solution {
+private:
+    ll getValue(int l, int r, SegmentTree& minST, SegmentTree& maxST, int n) {
+        int minEl = minST.query(l, r, n);
+        int maxEl = maxST.query(l, r, n);
+
+        return (ll)maxEl - (ll)minEl;
+    }
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        SegmentTree minST(nums, true);
+        SegmentTree maxST(nums, false);
+
+        priority_queue<tuple<ll, int, int>> pq;
+
+        for (int i = 0; i < n; i++) {
+            ll value = getValue(i, n - 1, minST, maxST, n);
+            pq.push({value, i, n - 1});
+        }
+
+        ll result = 0;
+
+        while (k--) {
+            auto [value, l, r] = pq.top();
+            pq.pop();
+
+            result += value;
+
+            ll nextval = getValue(l, r - 1, minST, maxST, n);
+
+            pq.push({nextval, l, r - 1});
+        }
+        return result;
+    }
+};
