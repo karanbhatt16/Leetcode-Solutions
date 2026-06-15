f9273006930527002ddeb91568d70e928a16d1e2
Karan Bhatt <karanbhatt160608@gmail.com>
2026-06-15 12:50:33 +0530

    Time: 100 ms (70.45%) | Memory: 30.9 MB (82.78%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     long long maximumProfit(vector<int>& prices, int K) {
         int n = prices.size();
-        vector<vector<vector<long long>>> dp(n, vector<vector<long long>> (3, vector<long long> (K + 1, 0)));
+        vector<vector<long long>> prev(3, vector<long long> (K + 1, 0));
+        vector<vector<long long>> curr(3, vector<long long> (K + 1, 0));
 
         for (int k = 1; k <= K; k++) {
-            dp[n - 1][1][k] = prices[n - 1];
-            dp[n - 1][2][k] = - prices[n - 1];
+            prev[1][k] = prices[n - 1];
+            prev[2][k] = - prices[n - 1];
         }
 
         for (int i = n - 2; i >= 0; i--) {
             for (int k = 1; k <= K; k++) {
-                dp[i][0][k] = max({dp[i + 1][0][k],
-                dp[i + 1][1][k] - prices[i],
-                dp[i + 1][2][k] + prices[i]});
+                curr[0][k] = max({prev[0][k],
+                prev[1][k] - prices[i],
+                prev[2][k] + prices[i]});
 
-                dp[i][1][k] = max(dp[i + 1][1][k],
-                dp[i + 1][0][k - 1] + prices[i]);
+                curr[1][k] = max(prev[1][k],
+                prev[0][k - 1] + prices[i]);
 
-                dp[i][2][k] = max(dp[i + 1][2][k], 
-                dp[i + 1][0][k - 1] - prices[i]);
+                curr[2][k] = max(prev[2][k], 
+                prev[0][k - 1] - prices[i]);
             }
+            prev = curr;
         }
-        return dp[0][0][K];
+        return prev[0][K];
     }
 };
