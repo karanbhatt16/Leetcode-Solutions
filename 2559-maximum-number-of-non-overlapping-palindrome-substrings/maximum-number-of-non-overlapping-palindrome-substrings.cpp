f85f8bbf125fa0a09317f2f4dd53224305a6b305
Karan Bhatt <karanbhatt160608@gmail.com>
2026-09-15 23:54:55 +0530

    Time: 2 ms (80.37%) | Memory: 8.8 MB (95.55%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -3,50 +3,39 @@ public:
     int maxPalindromes(string s, int k) {
         int n = s.length();
         int count = 0;
-
         if (k == 1) return n;
-
         int last = -1;
-
-        for (int i = 0; i < n; i++) {
-
+        for (int i = 0; i < n - k / 2; i++) {
+            // for odd
             int left1 = i - 1;
             int right1 = i + 1;
             int length1 = 1;
-
             while (left1 >= 0 && right1 < n && left1 > last &&
                    s[left1] == s[right1]) {
-
                 length1 += 2;
-
                 if (length1 >= k) {
                     break;
                 }
-
                 left1--;
                 right1++;
             }
 
+            // for even
             int left2 = i;
             int right2 = i + 1;
             int length2 = 0;
-
             while (left2 >= 0 && right2 < n && left2 > last &&
                    s[left2] == s[right2]) {
-
                 length2 += 2;
-
                 if (length2 >= k) {
                     break;
                 }
-
                 left2--;
                 right2++;
             }
 
-            if (length1 < k && length2 < k)
-                continue;
-
+            if (length1 < k && length2 < k) continue;
+            
             count++;
 
             if (length1 >= k && length2 >= k) {
@@ -56,15 +45,16 @@ public:
                     last = right2;
             }
 
+            // Only odd is valid
             else if (length1 >= k) {
                 last = right1;
             }
 
+            // Only even is valid
             else {
                 last = right2;
             }
         }
-
         return count;
     }
 };
